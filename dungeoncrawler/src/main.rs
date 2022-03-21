a97356dd47a8b24bded26dac7d28ff3d62d5ddac
Satish Gowda <satishgowda28@gmail.com>
2022-03-21 08:37:07 +0530

    map builder init

--- dungeoncrawler/src/main.rs
+++ dungeoncrawler/src/main.rs
@@ -1,11 +1,13 @@
 mod map;
 mod player;
+mod map_builder;
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH:i32 = 80;
     pub const SCREEN_HEIGHT:i32 = 50;
     pub use crate::map::*;
 		pub use crate::player::*;
+		pub use crate::map_builder::*;
 }
 use prelude::*;
 
