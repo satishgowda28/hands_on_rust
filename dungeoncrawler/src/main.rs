fc6efba937c323ba3ba5634ea8d8011408d30b8f
Satish Gowda <satishgowda28@gmail.com>
2022-04-04 08:25:40 +0530

    map builder added

--- dungeoncrawler/src/main.rs
+++ dungeoncrawler/src/main.rs
@@ -5,6 +5,8 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH:i32 = 80;
     pub const SCREEN_HEIGHT:i32 = 50;
+		pub const DISPLAY_WIDTH:i32 = SCREEN_WIDTH/2;
+		pub const DISPLAY_HEIGHT:i32 = SCREEN_HEIGHT/2;
     pub use crate::map::*;
 		pub use crate::player::*;
 		pub use crate::map_builder::*;
@@ -17,11 +19,14 @@ struct State {
 }
 impl State {
 	fn new() -> Self {
+		let mut rng = RandomNumberGenerator::new();
+		let map_builder = MapBuilder::new(&mut rng);
 		Self {
-			map: Map::new(),
-			player: Player::new(Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+			map: map_builder.map,
+			player: Player::new(map_builder.player_start)
 		}
 	}
+	//Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 }
 impl GameState for State {
 	fn tick(&mut self, ctx: &mut BTerm) {
@@ -32,9 +37,15 @@ impl GameState for State {
 	}
 }
 fn main() -> BError {
-  let context = BTermBuilder::simple80x50()
+  let context = BTermBuilder::new()
 		.with_title("Dungeon Crawler")
 		.with_fps_cap(30.0)
+		.with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+		.with_tile_dimensions(32, 32)
+		.with_resource_path("resources/")
+		.with_font("dungeonfont.png", 32, 32)
+		.with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+		.with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
 		.build()?;
 	main_loop(context, State::new())
 }
--- dungeoncrawler/src/map_builder.rs
+++ dungeoncrawler/src/map_builder.rs
@@ -3,16 +3,28 @@ const NUM_ROOMS:usize = 20;
 
 pub struct MapBuilder {
   pub map:Map,
-  pub room: Vec<Rect>,
+  pub rooms: Vec<Rect>,
   pub player_start: Point
 }
 
 impl MapBuilder {
+  pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+    let mut mb = MapBuilder{
+      map: Map::new(),
+      rooms: Vec::new(),
+      player_start: Point::zero()
+    };
+    mb.fill(TileType::WALL);
+    mb.build_random_rooms(rng);
+    mb.build_corridor(rng);
+    mb.player_start = mb.rooms[0].center();
+    mb
+  }
   fn fill(&mut self, tile: TileType) {
-    self.map.tiles.iter_mut().for_each(|t| *t = tile)
+    self.map.tiles.iter_mut().for_each(|t| *t = tile);
   }
   fn build_random_rooms(&mut self, rng: &mut RandomNumberGenerator) {
-    while self.room.len() < NUM_ROOMS {
+    while self.rooms.len() < NUM_ROOMS {
       let room = Rect::with_size(
         rng.range(1, SCREEN_WIDTH - 10),
         rng.range(1, SCREEN_HEIGHT - 10),
@@ -20,7 +32,7 @@ impl MapBuilder {
         rng.range(2, 10)
       );
       let mut overlap = false;
-      for r in self.room.iter() {
+      for r in self.rooms.iter() {
         if r.intersect(&room) {
           overlap = true;
         }
@@ -32,7 +44,7 @@ impl MapBuilder {
             self.map.tiles[idx] = TileType::FLOOR;
           }
         });
-        self.room.push(room);
+        self.rooms.push(room);
       }
     }
   }
@@ -53,7 +65,7 @@ impl MapBuilder {
     }
   }
   fn build_corridor(&mut self, rng: &mut RandomNumberGenerator) {
-    let mut rooms = self.room.clone();
+    let mut rooms = self.rooms.clone();
     rooms.sort_by(|a,b| a.center().x.cmp(&b.center().x));
 
     for(i, room) in rooms.iter().enumerate().skip(1) {
