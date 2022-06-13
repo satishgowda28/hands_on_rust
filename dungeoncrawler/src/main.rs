5d1652d0afffa94c887f5d6eb52fdc63b5c45312
Satish Gowda <satishgowda28@gmail.com>
2022-06-13 07:19:10 +0530

    changes

--- dungeoncrawler/src/main.rs
+++ dungeoncrawler/src/main.rs
@@ -57,21 +57,24 @@
 // 	main_loop(context, State::new())
 // }
 
+mod camera;
 mod map;
 mod map_builder;
 mod player;
-mod camera;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-    pub use crate::map::*;
-    pub use crate::player::*;
-    pub use crate::map_builder::*;
     pub use crate::camera::*;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
@@ -79,7 +82,7 @@ use prelude::*;
 struct State {
     map: Map,
     player: Player,
-    camera: Camera
+    camera: Camera,
 }
 
 impl State {
@@ -87,9 +90,9 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map : map_builder.map,
+            map: map_builder.map,
             player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start)
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
@@ -107,7 +110,7 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::new()// (1)
+    let context = BTermBuilder::new() // (1)
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // (2)
@@ -115,8 +118,7 @@ fn main() -> BError {
         .with_resource_path("resources/") // (4)
         .with_font("dungeonfont.png", 32, 32) // (5)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (6)
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, 
-            "dungeonfont.png") // (7)
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (7)
         .build()?;
 
     main_loop(context, State::new())
